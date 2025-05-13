fe7b892661edaff0ecbf8979fff559ddd953c500
shuangyi-jessica <liu951201@gmail.com>
2025-05-13 19:45:43 +0200

    modified some parts

--- provider.tf
+++ provider.tf
@@ -1,17 +1,18 @@
 # provider.tf
 
-provider "azurerm" {
-  features = {}
-}
-
 # Set the Azure Provider source and version being used
 terraform {
   required_version = ">= 0.14"
 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "~> 3.1.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+  # Configuration options
 }
