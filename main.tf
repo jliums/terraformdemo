8353b77906f0c8c41c3212b7262305f18f2590fc
jliums <152986477+jliums@users.noreply.github.com>
2025-05-14 10:43:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Create a Storage account
 resource "azurerm_storage_account" "terraform_sa" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
+  resource_group_name      = var.resource_group.name
+  location                 = var.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
