c85d8ee62138789d709a615d7091a5cfd309fe0c
jliums <152986477+jliums@users.noreply.github.com>
2025-05-14 10:42:09 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,8 @@
 # Input variable: Name of Storage Account
+variable "resource_group" {
+  default = "terraform_rg"
+}
+
 variable "storage_account_name" {
   description = "The name of the storage account. Must be globally unique, length between 3 and 24 characters and contain numbers and lowercase letters only."
   default     = "satfstate1405"
