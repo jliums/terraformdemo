79ba54ff8020d56b2bf6d34f045f87ed2fd2bf7e
jliums <152986477+jliums@users.noreply.github.com>
2025-05-14 12:14:08 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -29,7 +29,7 @@ provider "azurerm" {
   features {}
   # resource_provider_registrations = "core"
   #tenant_id            = "6607fd25-4a81-439b-bec8-a5f7e8227659"
-  subscription_id      = "0eb241f2-29fc-40f2-97b2-15579b4a8184"
+  #subscription_id      = "0eb241f2-29fc-40f2-97b2-15579b4a8184"
   #client_id           = "1625b1e9-2fc6-473c-8019-0666a345b1c0"
   #client_secret       = "VW38Q~J.kocub10Hj5h910bjNttmoGJp1DKYRc2j"
 }
