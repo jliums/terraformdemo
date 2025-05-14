b797979c389e428b5a97e01f9402e9e1613838e9
shuangyi-jessica <liu951201@gmail.com>
2025-05-14 09:06:11 +0200

    debugged main.yml

--- main.tf
+++ main.tf
@@ -20,6 +20,6 @@ resource "azurerm_storage_account" "terraform_sa" {
 # Create a Storage container
 resource "azurerm_storage_container" "terraform_state" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.terraform_state.name
+  storage_account_name  = azurerm_storage_account.terraform_sa.name
   container_access_type = "private"
 }
