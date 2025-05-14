af384262271d5767945081967a7332a6fc8c1af8
shuangyi-jessica <liu951201@gmail.com>
2025-05-14 09:00:53 +0200

    modified main.yml

--- provider.tf
+++ provider.tf
@@ -1,17 +1,31 @@
 # provider.tf
 
-provider "azurerm" {
-  features = {}
-}
+# provider "azurerm" {
+#   features = {}
+# }
 
-# Set the Azure Provider source and version being used
+# # Set the Azure Provider source and version being used
+# terraform {
+#   required_version = ">= 0.14"
+
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.1.0"
+#     }
+#   }
+# }
+
 terraform {
-  required_version = ">= 0.14"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.1.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "core"
 }
